335ba585d890609ff51df8a6e32f423b949595f8
erickfis@gmail.com <erickfis@gmail.com>
2017-03-25 23:57:29 -0300

    Primeira submissão - 2017-03-25 23:57

---
+++ analiseFin.R
@@ -0,0 +1,286 @@
+require(xlsx)
+require(data.table)
+require(dplyr)
+require(lubridate)
+require(ggplot2)
+require(RColorBrewer)
+
+
+
+#versão excel
+arquivo <- "data/start-2017-03-25.xls"
+
+
+
+dados <- read.xlsx(arquivo, sheetName = "Despesa", startRow = 2)
+
+# tratando o banco de dados
+
+##removendo NA, que neste caso só corresponde às linhas que não possuem data, são 
+## formatação do excel
+
+dadosT <- dados[complete.cases(dados),]
+
+
+
+str(dadosT) #dar uma olhada
+dadosT <- tbl_df(dadosT) #para usar dplyr
+
+names(dadosT) <- tolower(names(dadosT)) # para facilitar o uso das vars
+
+## acertando formatos de data e descrição
+## também adiciona as cols mes e ano, para facilitar o plot por painéis
+
+dadosM <- dadosT %>%    rename(tipo = tipo.de.despesa) %>%
+                        mutate(data = parse_date_time(data, "dmY"), 
+                               descrição = tolower(as.character(descrição)),
+                               tipo = factor(tolower(as.character(tipo))),
+                               mes = month(data, label=TRUE),
+                               ano = factor(year(data))
+                        ) %>%
+        
+# filtrando as informações que interessam, ou seja, somente da conta itau
+# e datas acima de 01-01-15, vamos considerar que eu não usava bem o app antes disso
+# e abaixo de 25-03-2017, quando o arquivo foi gerado (pq o db tem dados de 
+# "previsão de gastos" até o fim do ano
+
+                        filter(conta=="itau" &  data >= as.Date("2015-01-01") &
+                                                data <= as.Date("2017-03-25")) %>%
+## remove as colunas indesejadas
+                        select(data, ano, mes, tipo, descrição, valor)
+
+
+########fim da preparação do df, vamos começar #######
+
+
+dadosM <- tbl_df(dadosM)
+
+dadosM <- group_by(dadosM, tipo, ano, mes)
+
+write.csv(dadosM, "dadosM.csv")
+
+
+totais <- summarise(dadosM, total=sum(valor))
+
+# o gráfico
+
+# as cores:
+# Neste caso, temos 20 cat, mas somente 9 cores na paleta brewer padrao
+
+colourCount = length(unique(totais$tipo))
+getPalette = colorRampPalette(brewer.pal(9, "Set1"))
+
+
+plt <- ggplot(totais, aes(x=mes, y=total, group=tipo, colour=tipo))
+
+(plt +  geom_line() + geom_point() +
+                scale_colour_manual(values = getPalette(colourCount))+
+                facet_grid(ano ~.) + 
+                labs(title="Gastos por categoria", y="Total (R$)") + 
+                theme(plot.title = element_text(hjust = 0.5)) 
+                # guides(fill=FALSE)
+)
+
+
+dev.copy(png, file="plot1.png")
+dev.off()
+
+# olhando para o gráfico, vemos problemas em mar-2015, pagamentos
+# e em jan/mar-2017 - alimentação, 
+#vamos ver do q se trata, para talvez remover do gráfico
+
+sub1 <- with(dadosM, ano==2017 & tipo=="alimentação" & mes %in% c("Jan", "Mar"))
+sub2 <- with(dadosM, ano==2015 & mes=="Mar" & tipo=="pagamentos")
+
+arrange(dadosM[which(sub1),], desc(valor))
+# isso mostra que os valores de previsão não foram retirados após o final do mês,
+# podemos tirar do gráfico
+# vou filtrar os dados novamente para retirar todas as lihas onde 
+# descrição contenha "previsão"
+
+
+dadosM[which(sub2),]
+# consta um lançamento referente a pagamento de cc, mas não é assim que eu uso o programa
+# podemos tirar do gráfico limitando
+# vou filtrar os dados novamente para retirar todas as lihas onde 
+# descrição contenha "visa"
+
+
+dadosFil <- dadosM
+
+
+##teste do grep
+fonte <- c("previsao de gastos", "visa", "gastos", "gastos com visa")
+filtro <- which(!(grepl("[Pp]revisão", fonte) | grepl("[Vv]isa", fonte)))
+fonte[filtro] # grep ok
+
+fonte <- dadosFil$descrição
+filtro <- which(!(grepl("[Pp]revisão", fonte) | grepl("[Vv]isa", fonte)))
+
+#aplicando o filtro
+dadosFil <- dadosFil[filtro,]
+
+#agora podemos repetir o processo que vai gerar o plot
+
+dadosFil <- group_by(dadosFil, tipo, ano, mes)
+
+totais <- summarise(dadosFil, total=sum(valor))
+
+
+
+
+
+###novo gráfico
+# as cores:
+# Neste caso, temos 20 cat, mas somente 9 cores na paleta brewer padrao
+
+colourCount = length(unique(totais$tipo))
+getPalette = colorRampPalette(brewer.pal(9, "Set1"))
+
+
+plt <- ggplot(totais, aes(x=mes, y=total, group=tipo, colour=tipo))
+
+(plt +  geom_line() + geom_point() +
+                scale_colour_manual(values = getPalette(colourCount))+
+                facet_grid(ano ~.) + 
+                labs(title="Gastos por categoria", y="Total (R$)") + 
+                theme(plot.title = element_text(hjust = 0.5)) 
+        # guides(fill=FALSE)
+)
+
+
+dev.copy(png, file="plot2.png")
+dev.off()
+
+# analisando o 2nd gráfico, vemos que ainda há problemas com os lançamentos de 2017
+# vou ter que verificar os dados no servidor, para exportar novamente e recomeçar.
+# por hora, vamos apenas remover 2017
+
+totais <- totais[which(totais$ano %in% c(2015,2016)),]
+
+
+
+
+###novo gráfico
+# as cores:
+# Neste caso, temos 20 cat, mas somente 9 cores na paleta brewer padrao
+
+plt <- ggplot(totais, aes(x=mes, y=total, group=tipo, colour=tipo))
+
+
+(plt +  geom_line() + geom_point() +
+                scale_colour_manual(values = getPalette(colourCount))+
+                facet_grid(ano ~.) + 
+                labs(title="Gastos por categoria", y="Total (R$)") + 
+                theme(plot.title = element_text(hjust = 0.5)) 
+        # guides(fill=FALSE)
+)
+
+
+dev.copy(png, file="plot3.png")
+dev.off()
+#  tudo ok, vamos salvar os dados
+
+write.csv(dadosFil, "dados-filtrados.csv")
+
+write.csv(totais, "totais-2015a2016-12-31.csv")
+
+
+# # o próximo passo é estudar a variança, para ver quais categorias (tipos) 
+# devem ser analisadas com mais detalhe
+# por hora, vamos apenas olhar para as categorias mais promissoras, 
+# alimentação, pagamentos e corolla
+
+
+# alimentação
+
+plt.alim <- ggplot(totais[totais$tipo=="alimentação",], 
+                   aes(x=mes, y=total, colour=ano, group=ano))
+
+
+(plt.alim +  geom_line() + geom_point() +
+                facet_grid(ano ~.) + 
+                labs(title="Gastos - alimentação", y="Total (R$)") + 
+                theme(plot.title = element_text(hjust = 0.5)) 
+)
+
+dev.copy(png, file="plot-alim.png")
+dev.off()
+
+#analisar maio e julho
+
+dados.alim <- dadosFil %>% filter(mes %in% c("May","Jul") & tipo=="alimentação") %>%
+                arrange(ano, mes, desc(valor))
+
+View(dados.alim)
+
+
+#pagamentos
+
+plt.pag <- ggplot(totais[totais$tipo=="pagamentos",], 
+                   aes(x=mes, y=total, colour=ano, group=ano))
+
+
+(plt.pag +  geom_line() + geom_point() +
+                facet_grid(ano ~.) + 
+                labs(title="Gastos - pagamentos", y="Total (R$)") + 
+                theme(plot.title = element_text(hjust = 0.5)) 
+)
+
+dev.copy(png, file="plot-pag.png")
+dev.off()
+
+
+#analisar 2015 - pagamentos
+
+dados.pag <- dadosFil %>% filter(ano %in% c(2015) & tipo=="pagamentos") %>%
+        arrange(ano, mes, desc(valor))
+
+View(dados.pag)
+
+#corolla
+
+plt.cor <- ggplot(totais[totais$tipo=="corolla",], 
+                  aes(x=mes, y=total, colour=ano, group=ano))
+
+
+(plt.cor +  geom_line() + geom_point() +
+                facet_grid(ano ~.) + 
+                labs(title="Gastos - Corolla", y="Total (R$)") + 
+                theme(plot.title = element_text(hjust = 0.5)) 
+)
+
+dev.copy(png, file="plot-cor.png")
+dev.off()
+
+
+#analisar 2015
+
+dados.cor <- dadosFil %>% filter(ano %in% c(2015) & tipo=="corolla") %>%
+        arrange(ano, mes, desc(valor))
+
+View(dados.cor)
+
+
+
+
+
+##############
+# o plot abaixo arrancaria os pontos fora na unha:
+
+plt <- ggplot(totais, aes(x=mes, y=total, group=tipo, colour=tipo))
+
+(plt +  geom_line() + geom_point() + ylim(0,1750) +
+                scale_colour_manual(values = getPalette(colourCount))+
+                facet_grid(ano ~.) + 
+                labs(title="Gastos por categoria", y="Total (R$)") + 
+                theme(plot.title = element_text(hjust = 0.5)) 
+        # guides(fill=FALSE)
+)
+
+
+dev.copy(png, file="plotFinal.png")
+dev.off()
+       
+       
+       
---
+++ readme.md
@@ -0,0 +1,105 @@
+#Readme
+
+Análise de uma planilha de gastos, separados em 20 categorias, de 10/14 a 03/17.
+
+
+##O que este script faz
+
+- carrega o bd, neste caso, versão excel
+- faz o tratamento dos dados
+- grava o db para o disco - dadosM.csv
+- agrupa os dados por categoria (tipos de gastos) e faz um sumário calculando os totais
+- cria uma paleta de cores brewer para 20 categorias
+- plota os totais por categoria, um painel para cada ano
+- após o refinamento dos dados, através da análise dos plots 1 a 3, grava o db para o disco - totais.csv e dados-fitlrados.csv
+
+
+
+## Como são tratados os dados:
+- removendo NA, que neste caso só corresponde às linhas que não possuem data, são apenas 
+formatação do excel: complete.cases()
+- renomeando algumas variáveis, formatando seus nomes e seu conteúdo para lower case, para facilitar o uso das vars
+- acertando formatos de data e descrição, pacote lubridate
+- adicionando as cols mes e ano, para facilitar o plot por painéis
+- filtrando as informações que interessam, ou seja, somente da conta itau
+- datas acima de 01-01-15, vamos considerar que eu não usava bem o app antes disso
+- datas abaixo de 25-03-2017, quando o arquivo foi gerado (pq o db tem dados de 
+"previsão de gastos" até o fim do ano
+- removendo as colunas indesejadas
+
+
+## Análise exploratória
+
+### Plot1
+
+![plot1](plot1.png)
+
+Analisando o primeiro plot, vemos que há problemas em mar-2015, pagamentos, e em jan/mar-2017 alimentação: pontos fora do gráfico
+
+Fazendo subsets, olhamos mais de perto para estes dados e verificamos que os valores de previsão não foram retirados após o final de alguns meses, portanto podemos tirar estes pontos do gráfico. Além disso, consta um lançamento referente a pagamento de cc, mas não é assim que eu uso o programa.
+
+Assim, filtrar os dados novamente para retirar todas as lihas onde descrição contenha "previsão" ou "visa", usando grep.
+
+Agora podemos gerar o plot 2
+
+### Plot2
+
+![plot2](plot2.png)
+
+
+Analisando o 2nd gráfico, vemos que ainda há problemas com os lançamentos de 2017: valores altos de alimentação para jan e mar, 0 em fev.
+
+Vou ter que verificar os dados no servidor, para exportar novamente o db e recomeçar.
+
+Por hora, vamos apenas remover 2017 e fazer o plot 3
+
+### Plot3
+
+![plot3](plot3.png)
+
+
+Tudo ok, vamos gravar os dados em totais.csv e dados-filtrados.csv.
+
+O próximo passo é estudar as categorias que mais se destacam: alimentação, pagamentos e corolla, fazendo subset para cada categoria
+
+
+### Plot4 - Alimentação
+
+![alimentacao](plot-alim.png)
+
+Observa-se:
+
+- tendência a aumento de gastos em maio. Fazendo um subset dos dados, verifica-se que isso ocorre porque as compras para a páscoa são pagas em maio, cc
+- portanto, vigiar o cartão em abril
+- tendência a aumento de gastos em julho. Fazendo um subset dos dados, verifica-se que isso ocorreu em 2016 porque foram feitas 2 compras no mes, para aproveitar uma promoção. Vê-se que este gasto despenca até setembro, voltando a subir em agosto, próx compra
+- sobe e desce: as compras maiores não são feitas todo mes
+
+### Plot5 - Pagamentos
+
+![Pagamentos](plot-pag.png)
+
+Observa-se:
+
+- aumento bizarro em 2015 de fev a set. Fazendo um subset, verifica-se que isso ocorreu porque a cada vez que as metas de economia não eram alcançadas, o valor gasto a mais era lançado no mês seguinte como déficit, atingindo o pico em agosto. 
+- Por outro lado, verifica-se que nunca mais houve deficit. 
+- Em setembro de 2015 abrimos a loja de roupas da aline, o valor investido foi parcelado e lançado nesta categoria.
+
+### Plot6 - Corolla
+
+![Corolla](plot-cor.png)
+
+Observa-se:
+
+- valores altos de agosto a nov de 2015: Fazendo um subset, verifica-se que isso ocorreu porque em agosto foi renovado o seguro, em 4x, isso explica as altas até nov;
+- e em setembro foi feita uma manutenção de 10k, também parcelada em 4x
+- queda a partir de jul-16: venda do carro em julho
+
+
+# Conclusão
+
+Entre 2015 e 2016, verificamos que:
+
+- os valores mais altos estão em alimentação, pagamentos e corolla
+- deve-se tomar cuidado com os gastos para a páscoa
+- havia um déficit em 2015, mas foi superado
+- o seguro do carro era uma pancada, e as manutenções mantinham as médias de gasto mensal na casa dos R$500, mas esse problema acabou com a venda do carro.
