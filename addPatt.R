e711550b01ceef9f6e1f3c092c73c57a96612956
erickfis@gmail.com <erickfis@gmail.com>
2017-08-05 19:47:49 -0300

    add moradia

--- addPatt.R
+++
@@ -1,9 +0,0 @@
-set.seed(678910)
-for(i in 1:40){
-  # flip a coin
-  coinFlip <- rbinom(1,size=1,prob=0.5)
-  # if coin is heads add a common pattern to that row
-  if(coinFlip){
-    dataMatrix[i,] <- dataMatrix[i,] + rep(c(0,3),each=5)
-  }
-}
